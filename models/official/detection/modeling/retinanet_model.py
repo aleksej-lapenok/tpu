d8dce23b08f9574f1815cd37335440f4fe5e69ea
Aleksej Lapenok <live-penguin@yandex-team.ru>
2021-05-11 18:01:43 +0300

    fix start value of smooth_l1_s

--- models/official/detection/modeling/retinanet_model.py
+++ models/official/detection/modeling/retinanet_model.py
@@ -110,7 +110,7 @@ class RetinanetModel(base_model.BaseModel):
                                trainable=True)
 
     smooth_l1_s = tf.Variable(dtype=tf.float32, name='smooth_l1_s',
-                                   initial_value=-5.3,
+                                   initial_value=-3.75,
                                    trainable=True)
     num_positives_sum = tf.reduce_sum(labels['num_positives'])
     if self._focal_loss_normalizer_momentum > 0.0:
